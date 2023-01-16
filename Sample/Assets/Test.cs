7e6287d2e2d7ea74579ae7609345a078311a148d
neoseurae12 <87654809+neoseurae12@users.noreply.github.com>
2023-01-16 16:00:21 +0900

    C#) 정수와 정수의 나눗셈

---
+++ Sample/Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] points = { 83, 99, 52, 93, 15 };
+
+        int sum = 0;
+        for(int i=0; i<points.Length; i++)
+        {
+            sum += points[i];
+        }
+
+        int avgInt1 = sum / points.Length;
+        Debug.Log(avgInt1); // 68
+
+        float avgInt2 = sum / points.Length;
+        Debug.Log(avgInt2); // 68
+
+        float avgFloat = 1.0f * sum / points.Length;
+        Debug.Log(avgFloat); // 68.4
+    }
+}
