9eea20250120d0dbf48fc4b423c04916f048e3e1
neoseurae12 <87654809+neoseurae12@users.noreply.github.com>
2023-01-18 14:39:08 +0900

    운세 룰렛 게임 -- 오브젝트 배치 및 동작

---
+++ Roulette/Assets/RouletteController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed = 0; // 회전 속도
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 클릭하면 회전 속도를 설정함
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 10;
+        }
+
+        // 회전 속도만큼 룰렛을 회전시킴
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        // 룰렛을 감속시킴 (추가)
+        this.rotSpeed *= 0.98f;
+    }
+}
