15a26ee7ab19c576b3e60e439495917d8f32bb70
neoseurae12 <87654809+neoseurae12@users.noreply.github.com>
2023-01-28 22:08:35 +0900

    자동차 멈추기 게임 -- 거리 정보 표시하기

---
+++ SwipeCar/Assets/GameDirector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   // UI를 사용하는 데 필요함
+
+public class GameDirector : MonoBehaviour
+{
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+
+    void Start()
+    {
+        // 씬 안의 해당 오브젝트 찾아 넣기
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+    }
+
+    void Update()
+    {
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        if (length >= 0)
+        {
+            this.distance.GetComponent<Text>().text = "목표 지점까지 " + length.ToString("F2") + "m"; // 소수점 둘째 자리까지 표시
+        } else
+        {
+            this.distance.GetComponent<Text>().text = "게임 오버!";
+        }
+    }
+}
