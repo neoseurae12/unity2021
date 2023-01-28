420a09a951ddd15072ddae6fe2cb92d132c3908d
neoseurae12 <87654809+neoseurae12@users.noreply.github.com>
2023-01-28 18:16:28 +0900

    자동차 멈추기 게임 -- 클릭하면 자동차가 달리다 서서히 멈추는 스크립트

---
+++ SwipeCar/Assets/CarController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0;
+    Vector2 startPos;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))        // 마우스 왼쪽 버튼 클릭하면
+        {
+            this.speed = 0.2f;                  // 처음 속도
+        }
+
+        transform.Translate(this.speed, 0, 0);  // 이동
+        this.speed *= 0.98f;                    // 감속
+    }
+}
